12a83dd92e2521299800bc2e73c00ec94ba7a3d4
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2024-09-11 16:54:23 +0330

    orders screen base design

--- lib/screens/oreders/orders_screen.dart
+++ lib/screens/oreders/orders_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/common/const.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
     return Scaffold(
-      body: Center(
-        child: Text('Orders Screen'),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 10,
+          padding: EdgeInsets.symmetric(horizontal: Constants.primaryPadding),
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.all(12),
+              margin:
+                  EdgeInsets.symmetric(vertical: Constants.primaryPadding / 2),
+              decoration: BoxDecoration(
+                borderRadius: Constants.primaryRadius,
+                color: themeData.colorScheme.error,
+                boxShadow: Constants.primaryBoxShadow(context),
+              ),
+              child: Column(
+                children: [Text('سفارش')],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
