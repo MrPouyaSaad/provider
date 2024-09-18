b1e429f386f90dead74992d3f0529fb0d992a138
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2024-09-18 17:34:28 +0330

    CustomShadowBox

---
+++ lib/widgets/custom_shadow_box.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../common/const.dart';
+
+class CustomShadowBox extends StatelessWidget {
+  const CustomShadowBox({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final padding = Constants.primaryPadding / 2;
+    return Container(
+      padding: EdgeInsets.fromLTRB(padding, padding * 2, padding, padding),
+      decoration: BoxDecoration(
+        color: themeData.colorScheme.surface,
+        borderRadius: Constants.primaryRadius,
+        boxShadow: Constants.primaryBoxShadow(context),
+      ),
+      child: child,
+    );
+  }
+}
